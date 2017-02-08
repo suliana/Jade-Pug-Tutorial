9f6d844d37f2a86a25df97f84e976adee4df98ff
Suliana Abbay <sulianaabbay@Sulianas-MacBook-Pro.local>
2017-02-08 21:37:04 +0000

    Inital Jade tutorial

---
+++ jadetut.js
@@ -0,0 +1,7 @@
+function template(locals) {
+var buf = [];
+var jade_mixins = {};
+var jade_interp;
+
+buf.push("<!DOCTYPE html><html><head><title>Jade Tutorial</title><script src=\"https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js\"></script></head><body></body></html>");;return buf.join("");
+}
